eee8dde5c6e37a7e2dd9400dfdd23ae4d5ac3d58
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-12 02:00:09 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie]:languages
+     programmer_hash[:dennis_ritchie][:languages]
+     puts "[:languages]"
 end
 
 def adding_matz
